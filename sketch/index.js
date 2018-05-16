a4507e2412f001e689f2e6004a87e354a3b997b8
Alex Kempton <alex@funwithtriangles.net>
2018-05-16 22:32:54 +0100

    Tidy

--- sketch/index.js
+++ sketch/index.js
@@ -1,10 +1,24 @@
 import { moveTo, lineTo, dot } from '../lib'
 import drawPolygon from '../lib/utils/drawPolygon'
 
-moveTo('800', '10')
-lineTo('1000', '10')
-moveTo('800', '300')
-lineTo('1000', '300')
-dot('1000', '400')
+// Move pen and draw lines to coordinates
+moveTo('900', '800')
+lineTo('900', '200')
 
-drawPolygon()
+moveTo('1200', '800')
+lineTo('1200', '200')
+
+moveTo('900', '500')
+lineTo('1200', '500')
+
+moveTo('1500', '700')
+lineTo('1500', '200')
+
+// Draw a single dot (only for the brave!)
+dot('1500', '800')
+
+// Fun polygon util
+// center X, center Y, radius, num of vertices, rotation (in rad)
+drawPolygon(1300, -500, 80, 3)
+drawPolygon(1300, -500, 200, 3, Math.PI)
+drawPolygon(1300, -500, 300, 5)
